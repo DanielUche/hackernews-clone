2410c4ec482d6624dfc007d3877f5f04b7ccf0bd
danieluche <dank.uche@yahoo.com>
2019-12-09 19:58:30 +0100

    add jwt and authorization methods

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,7 +1,10 @@
+const { APP_SECRET, getUserId } = require("../utils");
+
 const newFeed = async (parent, args, ctx, info) => {
   const link = await ctx.prisma.createFeed(args);
   return link;
 };
+
 const updateFeed = async (parent, args, ctx) => {
   const { id } = args;
   delete args.id;
@@ -13,6 +16,7 @@ const updateFeed = async (parent, args, ctx) => {
   });
   return feed;
 };
+
 const deleteFeed = async (parent, args, ctx, info) => {
   const { id } = args;
   const feed = await ctx.prisma.deleteFeed({
@@ -21,8 +25,48 @@ const deleteFeed = async (parent, args, ctx, info) => {
   return feed;
 };
 
+async function signup(parent, args, context, info) {
+  // 1
+  const password = await bcrypt.hash(args.password, 10);
+  // 2
+  const user = await context.prisma.createUser({ ...args, password });
+
+  // 3
+  const token = jwt.sign({ userId: user.id }, APP_SECRET);
+
+  // 4
+  return {
+    token,
+    user
+  };
+}
+
+async function login(parent, args, context, info) {
+  // 1
+  const user = await context.prisma.user({ email: args.email });
+  if (!user) {
+    throw new Error("No such user found");
+  }
+
+  // 2
+  const valid = await bcrypt.compare(args.password, user.password);
+  if (!valid) {
+    throw new Error("Invalid password");
+  }
+
+  const token = jwt.sign({ userId: user.id }, APP_SECRET);
+
+  // 3
+  return {
+    token,
+    user
+  };
+}
+
 module.exports = {
   newFeed,
   updateFeed,
-  deleteFeed
+  deleteFeed,
+  signup,
+  login
 };
