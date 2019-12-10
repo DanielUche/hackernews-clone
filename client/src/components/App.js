aae450ee2b338ee48f500911dd28e9c1679f995f
danieluche <dank.uche@yahoo.com>
2019-12-10 22:34:06 +0100

    bootsrapped app navigation

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,27 @@
-import React from "react";
-import FeedList from "./FeedList";
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
-const App = () => <FeedList />;
+import Header from "./Header";
+import FeedList from "./FeedList";
+import CreateFeed from "./CreateFeed";
+
+class App extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div className="center w85">
+        <Header />
+        <div className="ph3 pv1 background-gray">
+          <Switch>
+            <Route exact path="/" component={FeedList} />
+            <Route exact path="/create" component={CreateFeed} />
+          </Switch>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
